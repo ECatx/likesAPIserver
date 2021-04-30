440e4e7bedb8a09f4c5314acc5759de77a7213c7
Eric Castellanos <eric@ericcastellanos.com>
2021-04-30 11:44:08 -0500

    setup

---
+++ src/helpers/db.js
@@ -0,0 +1,23 @@
+import knex from 'knex'
+
+const db = knex({
+  client: process.env.DB_TYPE,
+  connection: {
+    filename: process.env.DB_FILE,
+  },
+  useNullAsDefault: true,
+})
+
+const createTables = async () => {
+  const likesTableExists = await db.schema.hasTable('likes')
+  if (!likesTableExists) {
+    await db.schema.createTable('likes', (table) => {
+      table.increments('id').primary()
+      table.string('objectId')
+      table.string('userId')
+      table.timestamp()
+    })
+  }
+}
+
+createTables()
